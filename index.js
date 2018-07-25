f1ba08636a6572f3f7090c4dc3cdecf08f3c7788
Jeffrey Jiang <anon0503@gmail.com>
2018-07-25 04:14:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+let ridestart = 0;
+let distancefromhqinB = 0;
+function distanceFromHqInBlocks(ridestart){
+  if (ridestart > 42)
+  {
+    distancefromhqinB = ridestart - 42;
+    return distancefromhqinB;
+  }
+  else if (ridestart < 42)
+  {
+    distancefromhqinB = 42 - ridestart;
+    return distancefromhqinB;
+  }
+  else if (ridestart === 42)
+  {
+    distancefromhqinB = 0;
+    return distancefromhqinB;
+  }
+}
+
+function distancefromhqinFeet(ridestart){
+  if (ridestart > 42)
+  {
+    distancefromhqinF = (ridestart - 42) * 264;
+    return distancefromhqinF;
+  }
+  else if (ridestart < 42)
+  {
+    distancefromhqinF = (42 - ridestart) * 264;
+    return distancefromhqinF;
+  }
+  else if (ridestart === 42)
+  {
+    distancefromhqinF = 0;
+    return distancefromhqinF;
+  }
+}
